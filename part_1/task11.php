c75cbcf43762fef4831687ee22d93f0ba074ea46
nex641 <vviper5460@gmail.com>
2021-09-30 17:14:26 +0300

    Одиннадцатое задание

---
+++ part_1/task11.php
@@ -0,0 +1,26 @@
+<!-- 11.Напечатать те   элементы   последовательности   натуральных   чисел n0 ,n1,...,nm , которые делятся на сумму своих цифр.-->
+<?php
+require_once "functions.php";
+function isElement($n)
+{
+    $sum = 0;
+    $n2 = $n;
+    while ($n2 > 0) {
+        $number = $n2 % 10;
+        $sum += $number;
+        $n2 = floor($n2 / 10);
+    }
+    if (fmod($n, $sum) == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+$N = 300;
+for ($i = 0; $i < $N; $i++) {
+    if (isElement($i)) {
+        echo $i;
+    }
+}
+?>
