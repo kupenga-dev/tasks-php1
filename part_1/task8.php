bb43360bf3072ca519f635e00f12d01a159962f6
nex641 <vviper5460@gmail.com>
2021-09-30 15:24:05 +0300

    Восьмое задание

---
+++ part_1/task8.php
@@ -0,0 +1,29 @@
+<!-- 8.Получить все четырехзначные целые числа, в записи которых нет одинаковых цифр. -->
+<?php
+require_once "functions.php";
+function DoubleNumbers($N)
+{
+    if ($N > 9) {
+        $number = $N % 10;
+        $N2 = floor($N / 10);
+        while ($N2) {
+            if ($number === ($N2 % 10)) {
+                return true;
+            }
+            $N2 = floor($N2 / 10);
+        }
+        return DoubleNumbers(floor($N / 10));
+    } else {
+        return 0;
+    }
+}
+
+
+for ($i=1000; $i < 10000; $i++) { 
+    if(!DoubleNumbers($i))
+    {
+        echo $i;
+    }
+}
+
+?>
