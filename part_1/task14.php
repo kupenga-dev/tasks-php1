3c8ef7cd9f9fb25de8332f30c3c4ebb6243d49aa
nex641 <vviper5460@gmail.com>
2021-10-01 11:51:52 +0300

    Четырнадцатое задание

---
+++ part_1/task14.php
@@ -0,0 +1,34 @@
+<!-- 14.Парными  простыми числами называются два простых числа, разность  которых равна двум.
+Например,  3 и 5; 11 и 13. Найти 10  парных  простых чисел. -->
+<?php
+function isPrime($number)
+{
+    if ($number == 2) {
+        return true;
+    }
+    if ($number % 2 == 0) {
+        return false;
+    }
+    $i = 3;
+    $limit = (int) sqrt($number);
+    while ($i <= $limit) {
+        if ($number % $i == 0) {
+            return false;
+        }
+        $i += 2;
+    }
+    return true;
+}
+$k = 1;
+$count = 0;
+while ($count != 10) {
+    if(isPrime($k) && isPrime($k+2))
+    {
+        echo($k) . "  ";
+        echo($k+2);
+        echo "<pre>";
+        $count++;
+    }
+    $k+=2;
+}
+?>
