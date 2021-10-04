8575284ea215dbcfb073aeab526539443bf4da19
nex641 <vviper5460@gmail.com>
2021-10-04 13:48:42 +0300

    Исправлено игнорирование нуля в числе

--- part_1/task23.php
+++ part_1/task23.php
@@ -1,7 +1,7 @@
 <!-- 23.Дано  целое  число  N.  Преобразовать  число  так, чтобы его цифры следовали в порядке возрастания. -->
 <?php
 
-$N = 121434325;
+$N = 102140343250;
 $countN = floor(log10($N) + 1);
 $Nsorted = 0;
 
@@ -15,6 +15,11 @@ for ($i = $countN; $i > 0; $i--) {
         }
         $temp /= 10;
     }
+}
+if (floor(log10($Nsorted) + 1) != $countN)
+{
+    $pow = abs(floor(log10($Nsorted) + 1) - $countN);
+    $Nsorted = $Nsorted * (10 ** $pow);
 }
 echo $Nsorted;
 
