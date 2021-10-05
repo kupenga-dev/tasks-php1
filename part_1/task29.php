9820b9e8b152853ceb06bbcceddcfa992980378d
nex641 <vviper5460@gmail.com>
2021-10-05 11:46:24 +0300

    29-е задание

---
+++ part_1/task29.php
@@ -0,0 +1,64 @@
+<!-- 29.Для  каждого  числа  заданной  последовательности  натуральных   чисел   n0 ,n1,...,nm установить,  можно  ли  вычеркнуть  в  нем  некоторые  цифры, чтобы сумма оставшихся равнялась
+заданному числу к.   -->
+<?php
+// ini_set('max_execution_time', '100');
+require_once "functions.php";
+function Sum($number, $k)
+{
+    $sum = 0;
+    $secondNumber = $number;
+    $count = floor(log10($number));
+
+    while ($secondNumber > 0) {
+        $digit = $secondNumber % 10;
+        $secondNumber = floor($secondNumber / 10);
+        $sum += $digit;
+    }
+    if ($sum < $k) {
+        return false;
+    }
+    if ($sum == $k) {
+        $secondNumber = $number;
+        while ($secondNumber > 0) {
+            $digit = $secondNumber % 10;
+            if ($digit == 0) {
+                return true;
+            }
+            $secondNumber = floor($secondNumber / 10);
+        }
+        return false;
+    }
+    if ($sum > $k) {
+        $secondNumber = $number;
+        while ($number > 0) {
+            $digit = $number % 10;
+            if ($sum - $digit == $k) {
+                return true;
+            }
+
+            while ($secondNumber > 0) {
+                $digit2 = $secondNumber % 10;
+                if ($sum - ($digit + $digit2) == $k) {
+                    return true;
+                }
+                $secondNumber = floor($secondNumber / 10);
+            }
+            $number = floor($number / 10);
+        }
+    }
+}
+
+$k = 5;
+$n = 100;
+$m = 1500;
+
+for ($i = $n; $i < $m; $i++) {
+    if (Sum($i, $k)) {
+        echo $i . "  Можно";
+        echo "<pre>";
+    } else {
+        continue;
+    }
+}
+
+?>
