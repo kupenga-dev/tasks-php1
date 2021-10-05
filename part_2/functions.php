d655582a8e644be5d2aaf4ca77dd1831c7755835
nex641 <vviper5460@gmail.com>
2021-10-05 14:30:14 +0300

    6-е задание

---
+++ part_2/functions.php
@@ -0,0 +1,20 @@
+<?php
+// require_once "functions.php";
+function pr($s)
+{
+    echo "<pre>";
+    print_r($s);
+    echo "<pre>";
+}
+
+function GenerateRandomString()
+{
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = mb_strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i <= 8; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+
+}
